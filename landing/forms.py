ca9e17d4f51927fe59ebbc06ddbf77133734a17b
Diego Cáceres Cardoza <diegokaiser@gmail.com>
2022-11-29 23:46:46 +0100

    form validation

--- landing/forms.py
+++ landing/forms.py
@@ -1,8 +1,13 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Contact
 
 
-class CreateContactForm(ModelForm):
+class CreateContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['nombre', 'telefono', 'email']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'input__field input__field--haruki', 'required': ''}),
+            'telefono': forms.TextInput(attrs={'class': 'input__field input__field--haruki', 'required': '', 'pattern': '/^\d+$/;'}),
+            'email': forms.TextInput(attrs={'class': 'input__field input__field--haruki', 'required': '', 'pattern': '[a-z0-9]+@[a-z]+\.[a-z]{2,3}'}),
+        }
--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,64 @@
-console.log("script");
-
 var app = [];
 app.form = {
-  inputs: function () {},
-  validate: function () {},
-  form: function () {},
+  inputs: function () {
+    (function () {
+      var modalForm = document.querySelector(".modal_form");
+      var modalInput = modalForm.querySelectorAll("label");
+      for (const input of modalInput) {
+        input.classList.add("input__label", "input__label--haruki");
+      }
+
+      // trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+      if (!String.prototype.trim) {
+        (function () {
+          // Make sure we trim BOM and NBSP
+          var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+          String.prototype.trim = function () {
+            return this.replace(rtrim, "");
+          };
+        })();
+      }
+
+      [].slice
+        .call(document.querySelectorAll("input.input__field"))
+        .forEach(function (inputEl) {
+          // in case the input is already filled..
+          if (inputEl.value.trim() !== "") {
+            classie.add(inputEl.parentNode, "input--filled");
+          }
+
+          // events:
+          inputEl.addEventListener("focus", onInputFocus);
+          inputEl.addEventListener("blur", onInputBlur);
+        });
+
+      function onInputFocus(ev) {
+        classie.add(ev.target.parentNode, "input--filled");
+      }
+
+      function onInputBlur(ev) {
+        if (ev.target.value.trim() === "") {
+          classie.remove(ev.target.parentNode, "input--filled");
+        }
+      }
+    })();
+  },
+  validate: function () {
+    var buttonSend = document.querySelector(".js-sendForm");
+    buttonSend.setAttribute("disabled", "");
+  },
+  form: function () {
+    var buttonForm = document.querySelector(".js-activeContactModal");
+    var modal = document.querySelector(".modal");
+    var buttonClose = document.querySelector(".material-symbols-sharp");
+    buttonForm.addEventListener("click", function (e) {
+      modal.classList.add("active");
+    });
+    buttonClose.addEventListener("click", function (e) {
+      modal.classList.remove("active");
+    });
+  },
   build: function () {
-    console.log("app.form.build running");
     app.form.inputs();
     app.form.validate();
     app.form.form();
@@ -14,6 +66,5 @@ app.form = {
 };
 
 window.addEventListener("load", (event) => {
-  console.log("pagina cargada completamente");
   app.form.build();
 });
