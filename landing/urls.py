c4fbd2fbc4ef414b6537f79d37e667f72214d0a8
Diego Cáceres Cardoza <diegokaiser@gmail.com>
2022-12-01 23:45:42 +0100

    primero back

--- landing/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('login/', views.userlogin, name='login'),
-    path('lists/', views.lists, name='lists')
-]
